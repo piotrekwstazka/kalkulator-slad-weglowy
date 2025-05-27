1f70fec79afc8d44b7c1949237de33c7c495ecec
piotrekwstazka <piotrekkrajka@gmail.com>
2025-05-27 18:45:15 +0200

    Create dane.py

---
+++ dane.py
@@ -0,0 +1,21 @@
+wspolczynniki_emisji = {
+    "transport": {
+        "samochod_benzyna": 0.192,
+        "samochod_diesel": 0.171,
+        "pociag": 0.041,
+        "rower": 0.0
+    },
+    "dieta": {
+        "miesna": 5.0,
+        "wegetarianska": 2.0,
+        "weganska": 1.5
+    },
+    "energia": {
+        "prad_kwh": 0.39,
+        "woda_litr": 0.0003
+    },
+    "zakupy": {
+        "ubrania": 25.0,
+        "elektronika": 50.0
+    }
+}
